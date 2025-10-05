f0fd75666dce13b6189258bd5a2baf396d8cfd3e
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2025-10-04 18:54:28 -0700

    I fixed the routing paths for the home page to show up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/home.jsx';
 import Quiz from './components/Quiz/Quiz';
 
 function App() {
   return (
-    <>
-      <Quiz />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/quiz" element={<Quiz />} />
+    </Routes>
   );
 }
 
