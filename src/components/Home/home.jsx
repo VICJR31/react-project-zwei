4ff3143a815b57df446b587382840fb9e7868d2a
VICJR31 <73763182+VICJR31@users.noreply.github.com>
2025-10-04 19:21:13 -0700

    Got the Quiz button working!

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page of your React project.</p>
+      <Link to="/quiz">
+        <button>Start Quiz</button>
+      </Link>
     </div>
   );
 };
